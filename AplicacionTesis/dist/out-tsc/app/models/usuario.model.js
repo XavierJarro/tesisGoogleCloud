b998200f16c628109305ba2d2c6d0b639c7b0aee
XavierJarro <xa_cx4_vi@hotmail.com>
2022-04-05 00:26:04 -0500

    Revert "prueba"
    This reverts commit 39e4d50252151ad3b7574b2893a70222d93708bc.

--- AplicacionTesis/dist/out-tsc/app/models/usuario.model.js
+++
@@ -1,8 +0,0 @@
-export class UsuarioModel {
-    constructor() {
-        this.email = "";
-        this.password = "";
-        this.nombre = "";
-    }
-}
-//# sourceMappingURL=usuario.model.js.map
